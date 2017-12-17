3e906293e93d7e9d3a5e88c513f1c75b9a74391f
thegwan <gwan@princeton.edu>
2017-12-17 18:05:10 -0500

    enter filename

--- test.c
+++ test.c
@@ -5,8 +5,12 @@ int main()
 {
   FILE *fptr;
   int c;
+  char filename[100];
   printf("begin\n");
-  fptr = fopen("file.txt", "rw");
+  printf("enter filename: ");
+  scanf("%s", filename);
+
+  fptr = fopen(filename, "rw");
   printf("opened\n");
   if (fptr == NULL) {
     printf("fptr is null");
